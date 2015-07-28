7817079681b33158563ffc9b9be4b1fbd2957b51
Eric J. Holmes <eric@ejholmes.net>
2015-07-27 17:07:09 -0700

    Update hookshot.

--- Godeps/_workspace/src/github.com/ejholmes/hookshot/hookshot.go
+++ Godeps/_workspace/src/github.com/ejholmes/hookshot/hookshot.go
@@ -13,6 +13,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"golang.org/x/net/context"
 )
 
 const (
@@ -73,8 +75,24 @@ func (r *Router) Handler(req *http.Request) http.Handler {
 // ServeHTTP implements the http.Handler interface to route a request to an
 // appropriate http.Handler, based on the value of the X-GitHub-Event header.
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.ServeHTTPContext(context.Background(), w, req)
+}
+
+// contextHandler is an http handler that can propagate a context.Context.
+type contextHandler interface {
+	ServeHTTPContext(context.Context, http.ResponseWriter, *http.Request) error
+}
+
+// ServeHTTPContext implements the httpx.Handler interface.
+func (r *Router) ServeHTTPContext(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
 	h := r.Handler(req)
+
+	if h, ok := h.(contextHandler); ok {
+		return h.ServeHTTPContext(ctx, w, req)
+	}
+
 	h.ServeHTTP(w, req)
+	return nil
 }
 
 func (r *Router) notFoundHandler() http.Handler {
@@ -118,6 +136,11 @@ func Authorize(h http.Handler, secret string) *SecretHandler {
 
 // ServeHTTP implements the http.Handler interface.
 func (h *SecretHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	h.ServeHTTPContext(context.Background(), w, req)
+	return
+}
+
+func (h *SecretHandler) ServeHTTPContext(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
 	if h.Unauthorized == nil {
 		h.Unauthorized = DefaultUnauthorizedHandler
 	}
@@ -132,11 +155,16 @@ func (h *SecretHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 		if !ok {
 			h.Unauthorized.ServeHTTP(w, req)
-			return
+			return nil
 		}
 	}
 
+	if handler, ok := h.Handler.(contextHandler); ok {
+		return handler.ServeHTTPContext(ctx, w, req)
+	}
+
 	h.Handler.ServeHTTP(w, req)
+	return nil
 }
 
 // Signature calculates the SHA1 HMAC signature of body, signed by the secret.
