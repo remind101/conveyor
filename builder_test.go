e4d344bc7167a767bdc5d1c91f2744606e66340b
Eric J. Holmes <eric@ejholmes.net>
2015-10-16 15:43:52 -0700

    Add Builder test.

---
+++ builder_test.go
@@ -0,0 +1,25 @@
+package conveyor
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/remind101/conveyor/builder"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
+)
+
+func TestBuilder_Build(t *testing.T) {
+	b := new(mockBuilder)
+	bb := &Builder{
+		builder: b,
+	}
+
+	w := new(bytes.Buffer)
+	options := builder.BuildOptions{}
+
+	b.On("Build", w, options).Return("", nil)
+
+	_, err := bb.Build(context.Background(), w, options)
+	assert.NoError(t, err)
+}
