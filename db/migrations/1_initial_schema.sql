4d74e08593de0727d28c0e35bb83a554725d012e
Eric J. Holmes <eric@ejholmes.net>
2016-02-02 09:59:30 +0700

    Squash migrations

--- db/migrations/1_initial_schema.sql
+++ db/migrations/1_initial_schema.sql
@@ -4,19 +4,23 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE builds (
   id uuid NOT NULL DEFAULT uuid_generate_v4() primary key,
-  repository text,
+  seq SERIAL,
+  repository text NOT NULL,
   branch text,
-  sha text,
-  state text,
-  created_at timestamp without time zone default (now() at time zone 'utc'),
+  sha text NOT NULL,
+  state text NOT NULL,
+  created_at timestamp without time zone default (now() at time zone 'utc') NOT NULL,
   started_at timestamp without time zone,
   completed_at timestamp without time zone
 );
 
 CREATE TABLE artifacts (
   id uuid NOT NULL DEFAULT uuid_generate_v4() primary key,
+  seq SERIAL,
   build_id uuid NOT NULL references builds(id),
-  image text
+  image text NOT NULL,
+  repository text NOT NULL,
+  sha text NOT NULL
 );
 
 -- We should ensure that we only have 1 pending/building build for any given sha.
--- db/migrations/3_denormalize_artifacts.sql
+++
@@ -1,8 +0,0 @@
--- +migrate Up
-ALTER TABLE artifacts ADD COLUMN sha text;
-ALTER TABLE artifacts ADD COLUMN repository text;
-UPDATE artifacts AS a SET sha = b.sha, repository = b.repository FROM builds AS b WHERE a.build_id = b.id;
-
--- +migrate Down
-ALTER TABLE builds DROP COLUMN sha;
-ALTER TABLE artifacts DROP COLUMN repository;
